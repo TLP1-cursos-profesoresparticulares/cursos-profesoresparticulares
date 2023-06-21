01bf329078407148a16a17a3cbd79c44501b87f7
Piazza Carla Malena <carlamalenapiazzamail.com>
2023-06-21 09:11:07 -0300

    arreglos en app.js

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const cors = require ('cors');
 const path = require ('path');
 const { sequelize } = require('./src/database/connection');
 require('ejs');
+require('./src/models/typesUsers.model');
 
 
+sequelize.sync();
 sequelize.authenticate()
-    .then(() => console.log('Conectuwu'))
-    .catch((error) => console.log('Erruwu', error));
+    .then(() => console.log('Conectado'))
+    .catch((error) => console.log('Error', error));
 
 
 const app = express();
@@ -24,12 +26,10 @@ app.set('view engine', 'ejs');
 
 
 //rutas
-app.use(require('./src/routes/index.routes'));
-app.use(require('./src/routes/login.routes'));
-app.use(require('./src/routes/registro.routes'));
+app.use(require('./src/routes/views.routes'));
 app.use(require('./src/routes/auth.routes'));
 
 
 
 
-app.listen(3500);
+app.listen(3000, ()=>console.log('Servidor corriendo en el puerto 3000'));
